ac0d2547e9c68cacc6b0002dcedcd2b8f2bf33b2
artem_kopshik <artemktk@gmail.com>
2023-06-29 13:23:20 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿List<int> ArryaNumber(){
+    Console.Write("Inter multiple numbers separated by spase: ");
+    string NumbersStr = Console.ReadLine();
+    string[] NumberArr = NumbersStr.Split(' ');
+    List<int> list = new List<int>();
+    foreach(string number in NumberArr){
+        int Numeral;  
+        if(int.TryParse(number, out Numeral)){
+        list.Add(Numeral);
+        }
+        else {
+            Console.WriteLine("Enter correctly");
+        }
+    } 
+    return list; 
+}
+
+int GetEvenNums(List<int> list){
+    int count = 0;
+    for(int i = 0; i < list.Count; i++){
+        if(list[i]%2 == 0){
+            count += 1;
+        }
+    }
+    return count;
+}
+
+List<int> Courage =  ArryaNumber();
+int count = GetEvenNums(Courage);
+
+Console.WriteLine($"Even numbers, among the ones you entered {count}");
